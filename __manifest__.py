7adb309515b7b1d3423b1460d66636bfa5b23b6b
Felipe Montero Jiménez <fmj@xalachi.com>
2022-12-01 17:16:09 -0600

    commit inicial

---
+++ __manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+    "name" : "Website Terms and Conditions Odoo",
+    "version" : "14.0.0.0",
+    "category" : "eCommerce",
+    "price": 8,
+    "currency": 'EUR',
+    "depends" : ['base','website','website_sale'],
+    "author": "BrowseInfo",
+    "summary": 'terms and conditions on website tnc on website customizable terms and conditions on webshop terms and conditions on shop online terms and conditions online website terms conditions website terms Generator Terms of Use website eCommerce terms conditions',
+    "description": """
+Add Customizable Terms and Conditions on your Odoo Website.
+Using this apps able to add terms and condition on shop which user to agree before checkout
+add terms and condition on checkout page
+   webshop terms and conditions website
+   webstore terms and conditions webshop
+   website terms and conditions webstore
+   website Terms And Conditions Generator Terms of Use website
+   website Terms of Use webshop
+   website Terms of Use website
+   online terms and conditions online store
+   tnc on website
+   website tnc website
+   This Odoo apps is offers user to add customizable terms and conditions on their odoo website. Add "Restricted Payment" feature, if terms and conditions were not accepted. For odoo webshop policy or Ecommerce policy need to accepted by customer this app can enchance that feature in Odoo website page using this odoo module on Odoo ERP.
+
+   eCommerce terms and conditions eCommerce
+
+
+    """,
+    "website" : "https://www.browseinfo.in",
+    "data": [
+        'security/ir.model.access.csv',
+        'views/terms_conditions.xml',
+        'views/template.xml',
+    ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+    "images":['static/description/Banner.png'],
+    "live_test_url":'https://youtu.be/whdN5EA6GNA',
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
